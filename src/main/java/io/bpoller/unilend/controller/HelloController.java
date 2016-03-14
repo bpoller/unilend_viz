4b3e05e27a24e5976d5fabf498703d217c6a3505
bert <bpoller@ekito.fr>
2016-03-14 23:17:55 +0100

    Added logging

--- src/main/java/io/bpoller/unilend/controller/HelloController.java
+++ src/main/java/io/bpoller/unilend/controller/HelloController.java
@@ -1,18 +1,24 @@
 package io.bpoller.unilend.controller;
 
 import io.bpoller.unilend.model.SayHi;
+import org.slf4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class HelloController {
 
+    private Logger logger = getLogger(getClass());
+
     @RequestMapping(path = "/api/hello", method = GET)
     public ResponseEntity<SayHi> sayHello() {
+
+        logger.info("Hello {}", "world");
         return new ResponseEntity<>(new SayHi("Hello World"), OK);
     }
 
